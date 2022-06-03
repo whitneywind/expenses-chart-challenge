710153593a5ae285b2d42a31a4f4b87a380e8165
whitneywind <78887901+whitneywind@users.noreply.github.com>
2022-06-03 14:07:58 -0700

    bar chart without third party library

--- index.js
+++ index.js
@@ -1,4 +1,68 @@
+const url = 'data.json';
+const charts = document.querySelector('.charts');
+let highest;
+let currentDay = new Date().toLocaleDateString('en-us', { weekday: 'short' }).toLowerCase();
 
+
+fetch(url)
+  .then((res) => {
+      return res.json();
+  })
+  .then((data) => {
+    highest = data.reduce((acc, curr) => {
+      return acc > curr.amount ? acc : curr.amount
+    }, 0);
+    data.forEach((chart) => {
+      charts.appendChild(createChart(chart))
+    })
+    document.querySelectorAll('fadeIn').forEach((element, arr) => {
+      setTimeout(() => {
+        element.style.opacity = 1;
+        element.style.transform = 'translateY(0)'
+      }, (arr + 1) * 100 + 100)
+    })
+    document.querySelectorAll('.chart-bar').forEach((chartBar, arr) => {
+      setTimeout(() => {
+      const value = chartBar.getAttribute('data-value');
+      const percent = (value / highest) * 100;
+      chartBar.style.height = `${percent}%`;
+      chartBar.style.opacity = 1;
+      chartBar.style.transition = `height ${(percent * 0.3) / 100}s ease-in-out, opacity ${(percent * 0.3) / 100}s ease-in-out`
+    }, (arr + 1) * 100)
+  })
+})
+  .catch((err) => {
+    console.log(err)
+  })
+
+function createChart(chart) {
+	const div = document.createElement('div')
+	div.classList.add('chart')
+	if (chart.day == currentDay) {
+		div.classList.add('current')
+	}
+	div.innerHTML = `
+            <div class="chart-bar" data-value="${chart.amount}" ></div>
+            <div class="chart-day mini-caption medium-brown">${chart.day}</div>
+        `
+	return div
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 
 var xValues = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
 var yValues = [17.45, 34.91, 52.36, 31.07, 23.39, 43.28, 25.48];
@@ -22,4 +86,6 @@ new Chart("myChart", {
     }
   }
 });
+
+*/
 
